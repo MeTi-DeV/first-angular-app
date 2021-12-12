d7860c29a12afd6239744707e6c7d19f8fb9b352
MeTi-DeV <mahdi_zarei78@yahoo.com>
2021-12-12 15:02:18 +0330

    create my first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//comment 5 : to use new component we added should import that in app.module.ts
+// to use module for thsi  component
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
