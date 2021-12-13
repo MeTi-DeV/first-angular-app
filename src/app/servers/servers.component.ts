8f4b2c58380e878902cba5e186316c6c1a7ea8bf
MeTi-DeV <mahdi_zarei78@yahoo.com>
2021-12-13 15:48:32 +0330

    data binding , interpolation , events between components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,20 +1,24 @@
-//comment 1 : generate this Component by command "ng g c 'my- new-component-name'" it's automatically create all files for a new Component
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //comment 2 : several ways there are to define selector: as HTML Element
-  //as HTML Atribute
-  //as CSS Class
-  // selector: `[app-servers]` // as HTML attribute
-  // selector: `.app-servers` // as CSS class
   selector: 'app-servers',
-  //comment 3 : we can use other component HTML file directory and don't use itself HTML directory
-  //by use this inline component element for this example have to change templateUrl to template
-  template: `<app-server></app-server><app-server></app-server>`,
+
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  newCreateServerStatus = 'New Server Not Created !'
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  //comment 1 : an other topic in data binding is Event topic
+  // here create a function to onclickable button and show an Event on Webpage
+  onCreateServer(){
 
+this.newCreateServerStatus="New Server Created !";
+}
   ngOnInit(): void {}
 }
